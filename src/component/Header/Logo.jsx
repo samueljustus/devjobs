1ff22913d2443c11f77ee000e7c249ad08a27b8c
samuel <samie.justus@gmail.com>
2024-03-04 18:15:33 +0100

    moved logo to assets directory

--- src/component/Header/Logo.jsx
+++ src/component/Header/Logo.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to='/' className="flex items-center justify-center">
-      <img src="/public/img/logo.svg" alt="company logo" />
+    <Link to="/" className="flex items-center justify-center">
+      <img src="/src/assets/img/logo.svg" alt="company logo" />
       <h1 className="text-2xl font-helvetica font-medium">devJobs</h1>
     </Link>
   );
