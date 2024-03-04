68cdfa19cf015e13bc747b84d51fbcf37d76da42
samuel <samie.justus@gmail.com>
2024-03-04 09:32:45 +0100

    implementing split screen

--- src/page/PostJob/Preview.jsx
+++ src/page/PostJob/Preview.jsx
@@ -6,41 +6,44 @@ function Preview() {
 
   console.log(jobTitle);
   return (
-    <section className="h-screen font-Arial p-3">
-      <h2 className="italic text-xl font-semibold">Preview:</h2>
-      <p>Complete the form on the left to watch your listing come to life</p>
-      <div>
-        <div>
-          <h1>Job Title</h1>
-          <div>
-            <p>contract</p>
-            <p>fully</p>
+    <section className="h-screen font-Arial p-3 w-[60%]">
+      <p className="italic text-lg font-semibold">Preview:</p>
+      <p className="text-sm">Complete the form on the left to watch your listing come to life</p>
+      <div className="mt-20 flex flex-row justify-between items-center">
+        <div className ="flex flex-col item-center justify-start gap-2">
+          <h1 className="text-5xl">Job Title</h1>
+          <div className="flex flex-row gap-10 items-center">
+            <p className="italic text-sm ">job type</p>
+            <p className="italic text-sm ">fully remote</p>
           </div>
         </div>
-        <div>
-          <div>
+        <div className="flex flex-col justify-start">
+          <div className="flex flex-row justify-between items-center">
             <div>
-              <div>image</div>
+              <div className="w-14 h-14 bg-red-500">image</div>
             </div>
             <p>company name</p>
           </div>
-          <p>about your company</p>
-          <p>how should people apply</p>
+          <p className="m-2 text-sm">about your company</p>
+          <p className="m-2 text-lg">how should people apply</p>
         </div>
       </div>
       <div>
-        <p>About your role....</p>
-        <div>
+        {}
+      </div>
+      <div className="mt-20">
+        <p className="text-lg text-[] mb-10">About your role....</p>
+        <div className="mb-10">
           {/* unchanged */}
-          <h2>Experience</h2>
+          <h2 className="text-3xl font-semibold">Experience</h2>
           {/* unchanged */}
-          <p>required skills and experience...</p>
+          <p className="mt-6">required skills and experience...</p>
         </div>
-        <div>
+        <div className="">
           {/* unchanged */}
-          <h2>Salary and Perks</h2>
+          <h2 className="text-3xl font-semibold">Salary and Perks</h2>
           {/* unchanged */}
-          <p>salary and perks</p>
+          <p className="mt-6">salary and perks</p>
         </div>
       </div>
     </section>
