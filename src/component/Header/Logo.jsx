17cc1da05e85e0fb8f8bb4364c56bd7613c64a6a
samuel <samie.justus@gmail.com>
2024-03-04 18:31:12 +0100

    changed logo directory

--- src/component/Header/Logo.jsx
+++ src/component/Header/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/" className="flex items-center justify-center">
-      <img src="/public/vite.svg" alt="company logo" />
+      <img src="/img/logo.svg" alt="company logo" />
       <h1 className="text-2xl font-helvetica font-medium">devJobs</h1>
     </Link>
   );
